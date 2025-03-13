4a7ffc99163371c162befef98f9ae00dda5053c1
Naleash KNK <naleashwarakumaran@gmail.com>
2025-03-13 19:26:22 +0530

    Fix Websocket Routing Issue

--- lees_user_auth/asgi.py
+++ lees_user_auth/asgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from users.routing import routing
+from users.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lees_user_auth.settings')
 
@@ -13,7 +13,7 @@ application = ProtocolTypeRouter({
     "http": django_asgi_app,  # ✅ Handles HTTP requests
     "websocket": AuthMiddlewareStack(  # ✅ Handles WebSocket connections
          URLRouter(
-            routing.websocket_urlpatterns  # your websocket routes here
+            websocket_urlpatterns  # your websocket routes here
         )
     )
 })
